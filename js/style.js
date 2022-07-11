4f45ebf4f6d70c21f6882fedc2657e6cd7318aab
SarahOh92 <201124081oek@gmail.com>
2022-07-12 01:53:18 +0900

    first commit

---
+++ js/style.js
@@ -0,0 +1,12 @@
+// $(function () {
+//     $(".gnb_area").on("mouseenter", function(){
+//       $(".depth2").addClass("on");
+//     });
+      
+//     $(".gnb_area").on("mouseleave", function(){
+//       setTimeout(function(){
+//          $(".sub_bg, .depth2").removeClass("on");
+//          $(".gnb_area").removeClass("hover");
+//      }, 100);
+//   });
+// });
