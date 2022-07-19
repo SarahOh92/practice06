6b7b7b80074fa753038f71f6fb45fa3c91bd4b0a
SarahOh92 <201124081oek@gmail.com>
2022-07-19 10:07:34 +0900

    사이즈 수정

--- js/style.js
+++ js/style.js
@@ -1,12 +1,5 @@
-// $(function () {
-//     $(".gnb_area").on("mouseenter", function(){
-//       $(".depth2").addClass("on");
-//     });
-      
-//     $(".gnb_area").on("mouseleave", function(){
-//       setTimeout(function(){
-//          $(".sub_bg, .depth2").removeClass("on");
-//          $(".gnb_area").removeClass("hover");
-//      }, 100);
-//   });
-// });
+const btt = $('#top_btn');
+btt.click(function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},500)
+})
